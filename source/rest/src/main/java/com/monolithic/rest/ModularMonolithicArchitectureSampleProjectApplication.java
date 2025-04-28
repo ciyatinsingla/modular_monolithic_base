2643ef104f771613023650b4f59a95b25a412931
RATNENDDR.GIRRI <ratnenddr@chandikainnov.com>
2025-04-28 14:20:16 +0530

    changes added

---
+++ source/rest/src/main/java/com/monolithic/rest/ModularMonolithicArchitectureSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.monolithic.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModularMonolithicArchitectureSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModularMonolithicArchitectureSampleProjectApplication.class, args);
+	}
+
+}
